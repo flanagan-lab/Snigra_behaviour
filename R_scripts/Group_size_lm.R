030e3d51a83872d53985ea86fced37eb8e74ed4d
Sarah <spflanagan.phd@gmail.com>
2022-08-08 09:27:45 +1200

    Adding Fleur's script to analyse group size

---
+++ R_scripts/Group_size_lm.R
@@ -0,0 +1,199 @@
+
+# Subsetting data to only include male wiggles ------------------------------------------------------------
+
+### subsetting data to only include wanted information ###
+group<-final_data[c("behavior", "subject", "courtship_events", "modifier_2", "modifier_3", "Duration", "time_in_video", "Trial")]
+group1<-filter(group, behavior == "Wiggle")
+group2<- group1[!(group1$subject=="Female"),]
+group<- group2[!(group2$subject=="Second female"),]
+# Removing extreme values
+group<- group[!(group$Duration=="66.938" | group$Duration=="56.491"),] # removing 2 extreme duration values 
+# Removing any spaces in dataframe
+group<-data.frame(lapply(group,trimws))
+
+# changing things to factor or numeric
+group$modifier_3 <- as.factor(group$modifier_3)
+group$Duration <- as.numeric(group$Duration)
+
+# Creating a violin plot to look at distribution of data
+vioplot(group$Duration[group$modifier_3 == "2"], group$Duration[group$modifier_3 == "3"], group$Duration[group$modifier_3 == "4"],
+        group$Duration[group$modifier_3 == "5"], group$Duration[group$modifier_3 == "6"], names=c("2", "3", "4", "5", "6"))
+##vioplot(group$Duration ~ group$modifier_3,
+        #xlab = "Group size", ylab = "Duration of wiggle", main="Duration distribution")
+
+# Creating a scatterplot with all data points, colours for different court events 
+ggplot(data  = group,
+       aes(x = modifier_3,
+           y = Duration,
+           #col = courtship_events 
+             ))+
+  geom_point(size = 1.2,
+             alpha = .8,
+             position = "jitter")+
+  geom_line(data = fortify(groupsize), aes(x = modifier_3, y = .fitted))+
+  #geom_smooth(method = lm,
+            #se     = FALSE, 
+            #col    = "black",
+            ##size   = .5, 
+            #alpha  = .8)+ # to add regression line# to add some random noise for plotting purposes
+  theme_minimal()+
+  theme(legend.position = "none")+
+    labs(title = "Group size and male wiggle duration")
+
+
+
+# Linear Model - all groups separate, and extreme values included, removed random effect ------------------------------------------------------------
+
+group$courtship_events<- as.numeric(group$courtship_events)
+
+groupsize <-lm(Duration ~ modifier_3, data=group)
+  summary(groupsize)
+
+plot(groupsize, pch = 16, col = "blue") #Plot the results
+
+
+plot(Malegroup$residuals, pch = 16, col = "red")
+
+# second plot doesn't work? 
+plot(groupsize, which=1)
+plot(groupsize, which=2)
+
+# Q-Q plots 
+qqnorm(group$Durationlog, pch = 1, frame = FALSE)
+qqline(group$Durationlog, col = "steelblue", lwd = 2)
+
+
+# Just males lm -----------------------------------------------------------
+group1 <-lmer(Duration ~ modifier_3 + (1|courtship_events), data=group)
+summary(group1)
+plot(group1, which=1)
+plot(group1, which=2)
+
+
+# Q-Q plots 
+qqnorm(group$Duration, pch = 1, frame = FALSE)
+qqline(group$Duration, col = "steelblue", lwd = 2)
+
+# Log Duration ------------------------------------------------------------
+
+group$Durationlog <- log(group$Duration)
+Malegroup <-lmer(Durationlog ~ modifier_3 + (1|courtship_events), data=group)
+summary(Malegroup)
+# fixed effect parameter estimates (coefficients)
+coef(summary(Malegroup))
+
+
+plot(Malegroup$residuals, pch = 16, col = "red")
+
+plot(Malegroup, which=1)
+plot(Malegroup, which=2)
+plot(Malegroup, pch = 16, col = "blue")
+
+# Q-Q plot
+qqnorm(group$Durationlog, pch = 1, frame = FALSE)
+qqline(group$Durationlog, col = "steelblue", lwd = 2)
+
+# Getting results
+library(report)
+report(Malegroup)
+
+
+# Grouping >5 fish together in one group and removing extreme values --------
+
+group_dat <- group
+group_dat<- group_dat[order(group_dat$modifier_3),]
+group_dat$groupsize <- group_dat$modifier_3
+group_dat$groupsize <- as.numeric(as.character(group_dat$groupsize))
+group_dat$groupsize[group_dat$groupsize>4] <- 5
+group_dat$groupsize <- as.factor(group_dat$groupsize)
+str(group_dat)
+# plotting the data 
+ggplot(data  = group_dat,
+       aes(x = groupsize,
+           y = Durationlog,
+           col = courtship_events))+
+  geom_point(size = 1.2,
+             alpha = .8,
+             position = "jitter")+
+  geom_smooth(method = lm,
+              se     = FALSE, 
+              col    = "black",
+              size   = .5, 
+              alpha  = .8)+ # to add regression line# to add some random noise for plotting purposes
+  theme_minimal()+
+  theme(legend.position = "none")+
+  labs(title = "Group size and male wiggle duration")
+
+# linear model 
+library(lme4)
+library("lmerTest")
+group_dat$Durationlog <- log(group_dat$Duration)
+group2 <-lmer(Durationlog ~ groupsize + (1|courtship_events), data=group_dat)
+summary(group2)
+anova(group2)
+plot(group2, which=1)
+plot(group2, which=2)
+
+library(emmeans)
+e <-emmeans(group2, list(pairwise ~ groupsize), adjust = "tukey")
+
+write.csv(e, "C:\\Users\\Owner\\OneDrive - University of Canterbury\\Pipefish\\Pipefish_data\\Groupsize_posthoc.csv", row.names = FALSE)
+
+
+# Q-Q plot
+qqnorm(group_dat$Durationlog, pch = 1, frame = FALSE)
+qqline(group_dat$Durationlog, col = "steelblue", lwd = 2)
+
+
+# Getting results
+library(report)
+report(group2)
+
+0.02191 + 0.31921
+0.02191/0.34112
+0.31921/0.34112
+
+## not sure about this graph ####
+library("ggplot2")  
+library(broom.mixed)
+group2_augmented <- augment(group2)
+ggplot(fortify(group2_augmented), aes(groupsize, Durationlog, color=courtship_events)) +
+  stat_summary(fun.data=mean_se, geom="pointrange") +
+  stat_summary(aes(y=.fitted), fun=mean, geom="line")
+
+
+# Determining whether random effects is making a difference on the --------
+
+group$Durationlog <- log(group$Duration)
+
+# Create null model
+lmm.null <- lmer(Durationlog ~ 1 + (1|courtship_events), data = group) 
+summary(lmm.null)
+# 0.02115 +  0.31841 =  0.33956
+# 0.02115/ 0.33956 = 0.06228649 == ICC1 indicates that 6.2% of the variance in 
+  #'Duration' can be "explained" by courtship events
+anova(lmm.null)
+# Full model  
+library(lme4)
+library(lmerTest)
+Malegroup <-lmer(Durationlog ~ modifier_3 + (1|courtship_events), data=group)
+summary(Malegroup)
+anova(Malegroup)
+coef(Malegroup)
+plot(ranef(Malegroup))
+plot(Malegroup)
+# Comparing the models 
+anova(lmm.null, Malegroup)
+
+# Get p-values 
+anova(Malegroup)
+
+# Histogram of residuals 
+library(ggplot2)
+ggplot(data = group, aes(x = Malegroup$residuals)) +
+  geom_histogram(fill = 'steelblue', color = 'black') +
+  labs(title = 'Histogram of Residuals', x = 'Residuals', y = 'Frequency')
+
+# AIC values 
+AIC(lmm.null, Malegroup)
+
